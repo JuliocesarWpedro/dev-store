a50908751048b49b0e31bd1ee9ad298be7353910
Júlio César Wanderosfky Pedro <juliowanke@gmail.com>
2024-02-02 19:16:04 -0300

    fix: add API_URL in next.config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['github.com'],
   },
+  env: {
+    API_URL: 'http://localhost:3000',
+  },
 };
 
 module.exports = nextConfig;
--- src/data/api.ts
+++ src/data/api.ts
@@ -1,6 +1,5 @@
-
 export function api(path: string, init?: RequestInit) {
-  const baseUrl = "http://localhost:3000";
+  const baseUrl = process.env.API_URL || 'http://localhost:3000';
   const apiPrefix = '/api';
   const url = new URL(apiPrefix.concat(path), baseUrl);
 
