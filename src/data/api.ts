be6f48db0f200e246f9af61fe0a47237bf7f25ae
Júlio César Wanderosfky Pedro <juliowanke@gmail.com>
2024-02-02 18:49:59 -0300

    fix: remove VERCEL_URL AND APP_URL

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,15 +1,9 @@
-export function api(path: string, init?: RequestInit) {
-  const baseUrl =
-    process.env.VERCEL_URL ||
-    process.env.NEXT_PUBLIC_API_BASE_URL ||
-    'http://localhost:3000';
-  const apiPrefix = '/api';
+import { env } from '@/env';
 
-  try {
-    const url = new URL(apiPrefix.concat(path), baseUrl);
-    return fetch(url.toString(), init);
-  } catch (error) {
-    console.error('Error constructing URL:', error);
-    throw new Error('Error constructing URL');
-  }
+export function api(path: string, init?: RequestInit) {
+  const baseUrl = env.NEXT_PUBLIC_API_BASE_URL;
+  const apiPrefix = '/api';
+  const url = new URL(apiPrefix.concat(path), baseUrl);
+
+  return fetch(url, init);
 }
