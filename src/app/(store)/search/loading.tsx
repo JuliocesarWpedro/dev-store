ef4c560b15471b3aa558a88ceb7ffabaa391b922
Júlio César Wanderosfky Pedro <juliowanke@gmail.com>
2024-02-02 17:13:20 -0300

    feat: loading in search page

---
+++ src/app/(store)/search/loading.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import Skeleton from '@/components/skeleton';
+import { useSearchParams } from 'next/navigation';
+
+const Loading = () => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get('q');
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultado para: <span className="font-semibold">{query ?? ''}</span>
+      </p>
+      <div className="grid grid-cols-3 gap-6 slg:grid-cols-2 md:grid-cols-important">
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
