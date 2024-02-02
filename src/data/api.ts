209c38d5f10c4e0e4074d2ae7f49d0e5dc5992c2
Júlio César Wanderosfky Pedro <juliowanke@gmail.com>
2024-02-02 18:33:21 -0300

    fix: checks if it has a defined variable

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,8 +1,15 @@
-import { env } from '@/env';
-
 export function api(path: string, init?: RequestInit) {
-  const baseUrl = env.VERCEL_URL;
+  const baseUrl =
+    process.env.VERCEL_URL ||
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    'http://localhost:3000';
   const apiPrefix = '/api';
-  const url = new URL(apiPrefix.concat(path), baseUrl);
-  return fetch(url, init);
+
+  try {
+    const url = new URL(apiPrefix.concat(path), baseUrl);
+    return fetch(url.toString(), init);
+  } catch (error) {
+    console.error('Error constructing URL:', error);
+    throw new Error('Error constructing URL');
+  }
 }
